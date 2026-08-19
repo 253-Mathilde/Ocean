be66ae08b3a9bbda027f5e9f572fd074480605a9
253-Mathilde <arianebaby08@gmail.com>
2026-08-19 10:10:55 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const Score = document.getElementbyId("score");
-const Target = document.getElementbyId("target");
-let count = 0;
-Target.addEventListner("click", () => {
-  count++;
-  Score.textContent = count;
-  
-})
